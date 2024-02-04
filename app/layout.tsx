6d81a64e9b1c52b0b8d5e2758f2ced43eef8a684
kabornblack <shaibu.kabiru@email.com>
2024-02-04 17:38:19 +0200

    Create a fixed header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 
 import './globals.css'
+import Header from '@/components/Header'
+import { ThemeProvider } from '@/components/ThemeProvider'
 
 
 
@@ -16,7 +18,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-white dark:bg-[#1A1C29]">
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+          
+        <Header />
+
+        {children}
+        </ThemeProvider>
+        </body>
     </html>
   )
 }
