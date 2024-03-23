8bfe7858fa50f195ad38368a0f8b76aa564f0551
kabornblack <shaibu.kabiru@email.com>
2024-03-23 10:45:40 +0200

    clean and smooth

---
+++ app/genre/[id]/page.tsx
@@ -0,0 +1,35 @@
+// import React from 'react';
+
+import MoviesCarousel from "@/components/MoviesCarousel";
+import { getDiscoverMovies } from "@/lib/getMovies";
+
+type Props = {
+    params: {
+        id: string;
+    };
+
+    searchParams: {
+        genre: string;
+    };
+};
+
+async function GenrePage({ params: { id }, searchParams: { genre } }: Props ) {
+  const movies = await getDiscoverMovies(id);
+
+
+  return (
+    <div className="max-w-7xl mx-auto">
+      {/* AZURE OPENAI SERVICE SUGGESTION */}
+      <div className="flex flex-col space-y-4 mt-32 xl:-42">
+        <h1 className="text-6xl font-bold px-10"> 
+          Result for {genre}
+        </h1>
+
+        <MoviesCarousel title={`Genre`} movies={movies} isVertical />
+      </div>
+    </div>
+  );
+};
+
+
+export default GenrePage;
